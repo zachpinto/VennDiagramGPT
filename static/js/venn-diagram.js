a174bb5a49bd0b47e3525365c9dada7a0eb12084
Zachary TW Pinto <zachwalker.pinto@gmail.com>
2023-11-11 19:13:29 -0500

    radio buttons

--- static/js/venn-diagram.js
+++ static/js/venn-diagram.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var radios = document.querySelectorAll('input[type="radio"][name="setCount"]');
-    radios.forEach(function(radio) {
+    document.querySelectorAll('input[name="setCount"]').forEach(function(radio) {
         radio.addEventListener('change', function(event) {
-            var count = event.target.value;
-            var setTitleBoxes = document.getElementById('setTitleBoxes');
-            setTitleBoxes.innerHTML = ''; // Clear previous textboxes
+            var count = parseInt(event.target.value, 10);
+            var textFields = document.getElementById('textFields');
+            textFields.innerHTML = ''; // Clear previous fields
             for (var i = 0; i < count; i++) {
                 var input = document.createElement('input');
                 input.type = 'text';
-                input.placeholder = 'Give this set a title';
-                setTitleBoxes.appendChild(input);
+                input.placeholder = `Title for set ${i+1}`;
+                input.name = `setTitle${i}`;
+                textFields.appendChild(input);
             }
         });
     });
