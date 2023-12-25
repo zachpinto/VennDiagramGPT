1a63452ffe9b24061bff95d6b7ee96970979e665
Zachary TW Pinto <zachwalker.pinto@gmail.com>
2023-12-25 18:59:10 -0500

    OpenAI model

--- app.py
+++ app.py
@@ -12,23 +12,24 @@ openai.api_key = os.getenv('API_KEY')
 def index():
     return render_template('index.html')
 
-
 @app.route('/generate-text', methods=['POST'])
 def generate_text():
     data = request.json
     set_titles = data['setTitles']
 
     try:
-        # Generate text using GPT-4 (simplified example)
-        response = openai.Completion.create(
-            model="text-davinci-004",  # or the latest GPT-4 model
-            prompt="Generate a coherent text that combines these concepts: " + ", ".join(set_titles),
-            max_tokens=150
+        # Use the Chat Completions API
+        response = openai.ChatCompletion.create(
+            model=os.getenv('OPENAI_MODEL', 'gpt-4'),
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": " ".join(set_titles)}
+            ]
         )
-        return jsonify({"text": response.choices[0].text.strip()})
+        return jsonify({"text": response.choices[0].message['content']})
     except Exception as e:
+        app.logger.error(f'Error generating text: {e}')
         return jsonify({"error": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
